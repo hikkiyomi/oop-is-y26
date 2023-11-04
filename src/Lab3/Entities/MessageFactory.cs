7635ce84f40774e1f4b39a1cf5b898175c9155e4
hikkiyomi <367394@niuitmo.ru>
2023-11-04 06:26:14 +0300

    feat: add message factory

---
+++ src/Lab3/Entities/MessageFactory.cs
@@ -0,0 +1,17 @@
+using Itmo.ObjectOrientedProgramming.Lab3.Models;
+
+namespace Itmo.ObjectOrientedProgramming.Lab3.Entities;
+
+public class MessageFactory
+{
+    private const int StartingId = 1;
+    private int _freeId = StartingId;
+
+    public Message Create(
+        string head,
+        string body,
+        int priority)
+    {
+        return new Message(_freeId++, head, body, priority);
+    }
+}
