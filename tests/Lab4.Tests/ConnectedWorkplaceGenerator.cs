899fd237cf4691956d148b168c76827c3d542944
hikkiyomi <367394@niuitmo.ru>
2023-11-18 23:11:21 +0300

    fix(tests: changed generator

--- tests/Lab4.Tests/ConnectedWorkplaceGenerator.cs
+++ tests/Lab4.Tests/ConnectedWorkplaceGenerator.cs
@@ -1,6 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using Itmo.ObjectOrientedProgramming.Lab4.Entities;
+using Itmo.ObjectOrientedProgramming.Lab4.Entities.Contexts;
+using Itmo.ObjectOrientedProgramming.Lab4.Entities.States;
+using Itmo.ObjectOrientedProgramming.Lab4.Services.FileSystems;
 
 namespace Itmo.ObjectOrientedProgramming.Lab4.Tests;
 
@@ -9,7 +12,12 @@ public class ConnectedWorkplaceGenerator : IEnumerable<object[]>
     public IEnumerator<object[]> GetEnumerator()
     {
         var workplace = new Workplace();
-        workplace.Execute("connect /home/wawowewo/ --mode=local");
+
+        workplace.ChangeState(new ConnectedWorkplace(
+            workplace,
+            new MainContext(
+                new LocalFileSystem(),
+                "/home/wawowewo/")));
 
         yield return new object[]
         {
