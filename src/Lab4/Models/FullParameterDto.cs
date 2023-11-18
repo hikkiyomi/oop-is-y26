403849d9ad5f1bbbf878193dce009a92999447ae
hikkiyomi <367394@niuitmo.ru>
2023-11-18 18:38:26 +0300

    chore: removing unused files

--- src/Lab4/Models/FullParameterDto.cs
+++
@@ -1,8 +0,0 @@
-using Itmo.ObjectOrientedProgramming.Lab4.Entities.Contexts;
-
-namespace Itmo.ObjectOrientedProgramming.Lab4.Models;
-
-public record FullParameterDto(
-    string ShortName,
-    string FullName,
-    ParameterContext Context);
