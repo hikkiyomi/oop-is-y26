fcb706697fb29300e87c108c2502cc8c1a5bdaf2
hikkiyomi <367394@niuitmo.ru>
2023-11-18 21:56:08 +0300

    feat: add config handling

--- src/Lab4/Entities/TreeComponents/FileTypeWriter.cs
+++ src/Lab4/Entities/TreeComponents/FileTypeWriter.cs
@@ -1,9 +1,13 @@
+using Itmo.ObjectOrientedProgramming.Lab4.Services.Parsers;
+
 namespace Itmo.ObjectOrientedProgramming.Lab4.Entities.TreeComponents;
 
 public class FileTypeWriter : ITypeWriter
 {
+    private readonly ConfigParser _parser = new();
+
     public string GetSymbol()
     {
-        return "-";
+        return _parser.FindValueByKey("file") ?? "-";
     }
 }
